c1eff4c011b0169935d72faaa3d677b26acebe59
TAKASHI1218 <t61342006@yahoo.co.jp>
2026-01-04 13:47:42 +0900

    Coupon関連のDTOの作成

---
+++ API_Practice/Model/DTO/CouponCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace API_Practice.Model.DTO
+{
+    public class CouponCreateDTO
+    {
+        public string Name { get; set; }
+
+        public int Percent { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
--- API_Practice/Program.cs
+++ API_Practice/Program.cs
@@ -1,5 +1,6 @@
 using API_Practice.Data;
 using API_Practice.Model;
+using API_Practice.Model.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -52,29 +53,48 @@ app.MapGet("api/coupon/{id:int}", (int id, ILogger<Program> _logger) =>
 }).WithName("GetCouponById").Produces<Coupon>(200);
 
 // クーポン作成
-app.MapPost("api/coupon", ([FromBody] Coupon coupon, ILogger<Program> _logger) =>
+app.MapPost("api/coupon", ([FromBody] CouponCreateDTO coupon_C_DTO, ILogger<Program> _logger) =>
 {
     _logger.Log(LogLevel.Information, "クーポン作成");
-    if (string.IsNullOrEmpty(coupon.Name))
+    if (string.IsNullOrEmpty(coupon_C_DTO.Name))
     {
         return Results.BadRequest("クーポン名を入力してください");
     }
 
-    if (CouponStore.couponList.FirstOrDefault(x => x.Name.ToLower() == coupon.Name.ToLower()) != null)
+    if (CouponStore.couponList.FirstOrDefault(x => x.Name.ToLower() == coupon_C_DTO.Name.ToLower()) != null)
     {
         return Results.BadRequest("すでに使用されているクーポン名です");
     }
+    
+    // 1. CouponCreateDTOの値をCouponに設定
+    Coupon coupon = new()
+    {
+        IsActive = coupon_C_DTO.IsActive,
+        Name = coupon_C_DTO.Name,
+        Percent = coupon_C_DTO.Percent
+    };
 
+    // 2. Couponに追加
     coupon.Id = CouponStore.couponList.Max(x => x.Id) + 1;
-
     CouponStore.couponList.Add(coupon);
 
+    // 3. 外部公開用のデータ表示のためCouponをCoponDTOをに設定
+    CouponDTO couponDTO = new()
+    {
+        Id = coupon.Id,
+        IsActive = coupon.IsActive,
+        Name = coupon.Name,
+        Percent = coupon.Percent,
+        Created = coupon.Created
+    };
+
     // 201を返しGetCouponByIdルートよりデータ取得可能
-    return Results.CreatedAtRoute("GetCouponById", new { coupon.Id }, coupon);
+    return Results.CreatedAtRoute("GetCouponById", new { coupon.Id }, couponDTO);
     //※右記と同義「return Results.Created($"/api/coupon/{coupon.Id}",coupon);」
 
     // 作成に成功した場合は"201"作成できなかった場合は"400"を返す
-}).WithName("CreateCoupon").Accepts<Coupon>("application/json").Produces<Coupon>(201).Produces(400); 
+    // CouponCreateDTOを受け取りCouponDTOを返す
+}).WithName("CreateCoupon").Accepts<CouponCreateDTO>("application/json").Produces<CouponDTO>(201).Produces(400); 
 
 app.MapPut("api/coupon", () => {
 
