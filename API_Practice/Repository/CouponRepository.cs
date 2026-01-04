a33ac329219cb07cf4fa368a0171199949b34339
TAKASHI1218 <t61342006@yahoo.co.jp>
2026-01-04 17:28:46 +0900

    Repositoryの作成

---
+++ API_Practice/Repository/CouponRepository.cs
@@ -0,0 +1,52 @@
+﻿using API_Practice.Data;
+using API_Practice.Model;
+using API_Practice.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Practice.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<ICollection<Coupon>> GetAllCouponAsync()
+        {
+            return await _db.Coupons.ToListAsync();
+        }
+
+        public async Task<Coupon> GetCouponByIdAsync(int id)
+        {
+            return await _db.Coupons.Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Coupon> GetCouponByNameAsync(string couponName)
+        {
+            return await _db.Coupons.Where(x => x.Name.ToLower() == couponName.ToLower()).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(Coupon coupon)
+        {
+            await _db.Coupons.AddAsync(coupon);
+        }
+
+        public void UpdateCoupon(Coupon coupon)
+        {
+            _db.Coupons.Update(coupon);
+        }
+
+        public void RemoveCoupon(Coupon coupon)
+        {
+            _db.Coupons.Remove(coupon);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ API_Practice/Repository/IRepository/ICouponRepository.cs
@@ -0,0 +1,21 @@
+﻿using API_Practice.Model;
+
+namespace API_Practice.Repository.IRepository
+{
+    public interface ICouponRepository
+    {
+        Task<ICollection<Coupon>> GetAllCouponAsync();
+
+        Task<Coupon> GetCouponByIdAsync(int id);
+
+        Task<Coupon> GetCouponByNameAsync(string couponName);
+
+        Task CreateAsync(Coupon coupon);
+
+        void UpdateCoupon(Coupon coupon);
+
+        void RemoveCoupon(Coupon coupon);
+
+        Task SaveAsync();
+    }
+}
