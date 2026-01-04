687118d468448f3d8c61866ed7184b57f3e16352
TAKASHI1218 <t61342006@yahoo.co.jp>
2026-01-04 19:56:49 +0900

    ログイン処理の作成

--- API_Practice/Repository/AuthRepository.cs
+++ API_Practice/Repository/AuthRepository.cs
@@ -3,6 +3,10 @@ using API_Practice.Model;
 using API_Practice.Model.DTO;
 using API_Practice.Repository.IRepository;
 using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace API_Practice.Repository
 {
@@ -10,11 +14,15 @@ namespace API_Practice.Repository
     {
         private readonly ApplicationDbContext _db;
         private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+        private string secretKey;
 
-        public AuthRepository(ApplicationDbContext db, IMapper mapper)
+        public AuthRepository(ApplicationDbContext db, IMapper mapper,IConfiguration configuration)
         {
             _db = db;
             _mapper = mapper;
+            _configuration = configuration;
+            secretKey = _configuration.GetValue<string>("ApiSettings:Secret");
         }
 
         public bool IsUniqueUser(string username)
@@ -26,9 +34,40 @@ namespace API_Practice.Repository
             return false;
         }
 
-        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers.SingleOrDefault(x => x.UserName == loginRequestDTO.UserName
+                           && x.Password == loginRequestDTO.Password);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            // JWT作成のためのハンドラーを準備
+            var tokenHandler = new JwtSecurityTokenHandler();
+            // Secretをバイト配列に変換
+            var key = Encoding.ASCII.GetBytes(secretKey);
+            // JWTの中身を設定 - Claims(Name,Role)、期限、署名方式
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name,user.UserName),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            // トークン作成、LoginResponseDTOの作成
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new()
+            {
+                User = _mapper.Map<UserDTO>(user),
+                Token = new JwtSecurityTokenHandler().WriteToken(token)
+            };
+
+            return loginResponseDTO;
         }
 
         public UserDTO Register(RegisterationRequestDTO requestDTO)
